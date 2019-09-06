5a8826be471d63192a4c30b32216853ab89ee142
Shuang Xia <shuangx@stradigi.ai>
2019-09-06 15:32:25 -0400

    add eslint

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const path = require('path');
 
-// 创建中间件
 const app = express();
-// 用express提供的静态目录的中间件
-// 换句话说，也就是加载public中的index.html文件， 它是一个本地文件
-// app.use(express.static(path.join(__dirname, 'public')));
 
-// 动态请求
-app.get('/user', (req, res) => {
-  res.send('I will send you user list later');
-  res.end();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my nodemon API');
 });
 
-app.listen(3000, () => {
-  console.log('http://127.0.0.1:3000');
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
 });
